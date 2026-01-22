f600e17ecb8b2f3e7f0e35618ccabd6e455ecb8a
ajaj677 <ajajm677@gmail.com>
2026-01-22 19:20:46 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+const App = () => {
+  return (
+    <div className='bg-black text-white'>
+        <Navbar/>
+
+
+
+
+
+
+
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Navbar = () => {
+  return (
+    <div className='flex p-5 justify-center w-full'>
+      <div className='flex border-3 border-solid border-[#36363b] py-2 px-5 rounded-2xl justify-between gap-12 text-white bg-[#262629] items-center'>
+        <Link to='/'>
+          <span className='flex items-center font-bold gap-2'>
+            <img 
+              src='/pfp.jpg' 
+              alt='ajaj.dev' 
+              className='w-10 h-10 object-cover rounded-full transition duration-500  blur-0 scale-100 ' 
+            />
+            Ajaj
+          </span>
+        </Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='https://discord.gg/mr3S7SVjMc' target='_blank'>Discord</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
