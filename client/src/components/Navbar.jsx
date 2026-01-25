06b25be0863ccc1fcde9b9f6f9a5f90a8c71fbc2
ajaj677 <ajajm677@gmail.com>
2026-01-25 21:33:28 +0530

    style: updated style of navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 const Navbar = () => {
   return (
     <div className='flex p-8 text-xs justify-center w-full'>
-      <div className=' flex border-2 border-solid border-[#36363b] py-2 px-4 rounded-2xl justify-between gap-4 text-white bg-[#262629] items-center'>
+      <div className='text-[#f0f6fc] flex border-[1.5px] border-solid border-[#36363b] py-2 px-4 rounded-2xl justify-between gap-4 text-white bg-[#262629] items-center'>
         <Link to='/'>
           <span className='flex text-sm items-center  gap-2'>
             <img 
