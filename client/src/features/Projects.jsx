67fe858c107682d894a732bfba77c72593b3d5fa
ajaj677 <ajajm677@gmail.com>
2026-01-27 23:41:38 +0530

    style: update the typo of card

--- client/src/features/Projects.jsx
+++ client/src/features/Projects.jsx
@@ -4,6 +4,8 @@ import JSLogo from '../assets/js.svg'
 import NodeJSLogo from '../assets/nodejs.svg'
 import MongoDBLogo from '../assets/mongodb.svg'
 import { FaGithub } from 'react-icons/fa';
+import { FaAws } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
 import { LuSquareArrowOutUpRight } from "react-icons/lu";
 
 
@@ -19,8 +21,19 @@ const Projects = () => {
                     <div className="bg-linear-to-tr bg-[#1a1e26] to-[#15191f] flex justify-between p-5 rounded-lg">
                         
                         {/* holds data */}
-                        <div className='flex flex-col gap-1'>
-                            <h2 className="text-2xl  font-bold">SpeechFlow</h2>
+                        <div className='flex flex-col flex-1 gap-0.5'>
+
+                            {/* header */}
+                            <div className='flex justify-between items-center '>
+                                
+                                <h2 className="text-2xl font-bold">SpeechFlow</h2>
+                                {/* live links */}
+                                <span className='flex justify-center text-[#9198a1] items-center text-2xl gap-3 '>
+                                    <a className='hover:text-[#f0f6fc]' href='https://github.com/ajajm/yt-converter' target='_blank'><FaGithub /></a>
+                                    <a className='hover:text-[#f0f6fc] active:scale-85' href="https://" target="_blank"><LuSquareArrowOutUpRight /></a>
+                                 </span>
+                            </div>
+
                             <p className='text-[#9198a1] font-light text-sm '>Real time text to speech bot</p>
                             <p className='text-[#9198a1] font-normal text-sm leading-snuh mt-4'>
                                 A minimal discord bot that converts text messages into speech and plays them in voice channels.
@@ -29,19 +42,14 @@ const Projects = () => {
                             <p className='text-[#9198a1] font-bold text-sm leading-snuh mt-4'>Tech Stack</p>
 
                             {/* tect stack logo */}
-                            <div className='flex py-2 flex-row text-sm gap-3'>
-                                    <img src={ReactLogo} className='w-5 h-5 inline mr-2 ' alt="React" />
-                                    <img src={JSLogo} className='w-5 h-5 inline mr-2 ' alt="React" />
-                                    <img src={NodeJSLogo} className='w-5 h-5 inline mr-2 ' alt="React" />
+                            <div className='flex mt-2 flex-row text-sm gap-3'>
                                     <img src={MongoDBLogo} className='w-5 h-5 inline mr-2 ' alt="React" />
+                                    <SiExpress className='w-5 h-5 inline mr-2' />
+                                    <img src={ReactLogo} className='w-5 h-5 inline mr-2 ' alt="React" />
+                                    <img src={NodeJSLogo} className='w-5 h-5 inline mr-2 ' alt="React" />
                             </div>
                         </div>
 
-                        {/* live links */}
-                        <span className='flex justify-center text-[#9198a1] items-center mb-auto text-2xl gap-3 '>
-                            <a className='hover:text-[#f0f6fc] active:scale-85' href="https://github.com/ajajm/Speechflow" target='_blank'><FaGithub className /></a>
-                            <a className='hover:text-[#f0f6fc] active:scale-85' href="https://speechflow.me/" target="_blank"><LuSquareArrowOutUpRight /></a>
-                        </span>
                     </div>
                 </div>
 
@@ -50,8 +58,16 @@ const Projects = () => {
                     <div className="bg-linear-to-tr bg-[#1a1e26] to-[#15191f] flex justify-between p-5 rounded-lg">
                         
                         {/* holds data */}
-                        <div className='flex flex-col gap-1'>
-                            <h2 className="text-2xl  font-bold">Ytm Bit</h2>
+                        <div className='flex flex-col flex-1 gap-0.5'>
+                            {/* header */}
+                            <div className='flex justify-between items-center '>
+                                <h2 className="text-2xl font-bold">YTM Bit</h2>
+                                {/* live links */}
+                                <span className='flex justify-center text-[#9198a1] items-center text-2xl gap-3 '>
+                                    <a className='hover:text-[#f0f6fc]' href='https://github.com/ajajm/yt-converter' target='_blank'><FaGithub /></a>
+                                    <a className='hover:text-[#f0f6fc] active:scale-85' href="https://" target="_blank"><LuSquareArrowOutUpRight /></a>
+                                 </span>
+                            </div>
                             <p className='text-[#9198a1] font-light text-sm '>A simple media converter</p>
                             <p className='text-[#9198a1] font-normal text-sm leading-snuh mt-4'>
                                 Convert and download YouTube videos as MP3 or MP4 easily. Built using yt-dlp, this tool provides  a simple yet powerful <br />backend with a minimal frontend interface.
@@ -59,18 +75,13 @@ const Projects = () => {
                             <p className='text-[#9198a1] font-bold text-sm leading-snuh mt-4'>Tech Stack</p>
 
                             {/* tect stack logo */}
-                            <div className='flex py-2 flex-row text-sm gap-3'>
+                            <div className='flex mt-2 flex-row  text-sm gap-3'>
+                                    <img src={MongoDBLogo} className='w-5 h-5 inline mr-2 ' alt="React" />
+                                    <SiExpress className='w-5 h-5 inline mr-2' />
                                     <img src={ReactLogo} className='w-5 h-5 inline mr-2 ' alt="React" />
-                                    <img src={JSLogo} className='w-5 h-5 inline mr-2 ' alt="React" />
                                     <img src={NodeJSLogo} className='w-5 h-5 inline mr-2 ' alt="React" />
                             </div>
                         </div>
-
-                        {/* live links */}
-                        <span className='flex justify-center text-[#9198a1] items-center mb-auto text-2xl gap-3 '>
-                            <a className='hover:text-[#f0f6fc]' href='https://github.com/ajajm/yt-converter' target='_blank'><FaGithub /></a>
-                            <a className='hover:text-[#f0f6fc] active:scale-85' href="https://" target="_blank"><LuSquareArrowOutUpRight /></a>
-                        </span>
                     </div>
                 </div>
 
